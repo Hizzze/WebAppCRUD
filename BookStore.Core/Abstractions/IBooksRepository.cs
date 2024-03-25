f5b83812b4be7dbdec43eac299be56831825b55f
Vlad Syzov <hizes5050@gmail.com>
2024-03-25 18:44:57 +0100

    Docker+EFcore+CRUD

---
+++ BookStore.Core/Abstractions/IBooksRepository.cs
@@ -0,0 +1,11 @@
+using BookStore.Core.Models;
+
+namespace BookStore.DataAccess.Repositories;
+
+public interface IBookRepository
+{
+    Task<Guid> Create(Book book);
+    Task<Guid> Delete(Guid id);
+    Task<List<Book>> Get();
+    Task<Guid> Update(Guid id, string title, string description, decimal price);
+}
